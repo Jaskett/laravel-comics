253bf84114442a424296e678eda91e3c20f86c7e
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-10 00:17:20 +0100

    Bluesection completato

---
+++ resources/views/components/bluesection.blade.php
@@ -0,0 +1,24 @@
+<section class="merchandise">
+    <ul class="container">
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+            <a href="#">DIGITAL COMICS</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+            <a href="#">DC MERCHANDISE</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+            <a href="#">SUBSCRIPTION</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+            <a href="#">COMIC SHOP LOCATOR</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+            <a href="#">DC POWER VISA</a>
+        </li>
+    </ul>
+</section>
